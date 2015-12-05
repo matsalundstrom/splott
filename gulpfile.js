becea1d40ca96bba4e26fd71b5199d013976810a
Mats Lundström <mats.a.lundstrom@gmail.com>
2015-12-05 14:54:48 +0100

    Added linting

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,20 @@
+/*global require*/
 var gulp = require('gulp');
+var eslint = require('gulp-eslint');
+
+
+gulp.task('eslint', function () {
+  'use strict';
+
+  return gulp
+    .src([
+      './src/**/*js',
+      './*.js'
+		])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failOnError());
+});
 
 gulp.task('default', function () {
   'use strict';
--- src/server/app.js
+++ src/server/app.js
@@ -1,11 +1,16 @@
+/*global require*/
 var express = require('express');
 
 var app = express();
 
 app.get('/', function (request, response) {
+	'use strict';
+
 	response.send('Hello World!');
 });
 
-app.listen('3000', function() {
+app.listen('3000', function () {
+  'use strict';
+
 	console.log('Express app stated on port 3000');
 });
