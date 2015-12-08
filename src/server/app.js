10978871e3bf79eaac6a80bd0590dd04d9057fc1
Mats Lundström <mats.a.lundstrom@gmail.com>
2015-12-08 22:02:34 +0100

    Beginning of a rest api

--- src/server/app.js
+++ src/server/app.js
@@ -3,6 +3,19 @@ var express = require('express');
 
 var app = express();
 
+var splottRouter = express.Router();
+
+splottRouter.route('/books')
+	.get(function(request, response) {
+		'use strict';
+		
+		var responseJson = {hello: 'this is my api'};
+		
+		response.json(responseJson);
+	});
+	
+app.use('/api', splottRouter);
+
 app.use(express.static('./src/client/'));
 app.use(express.static('./'));
 app.use(express.static('./tmp'));
@@ -11,7 +24,7 @@ app.use('/*', express.static('./src/client/index.html'));
 app.get('/', function (request, response) {
 	'use strict';
 
-	response.send('Hello World!');
+	response.send('Hello Big World!');
 });
 
 app.listen('3000', function () {
